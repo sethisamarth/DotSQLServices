f765dec999e9880661827e1de9842ffb3403d746
Samarth <sethisamarth1@gmail.com>
2022-01-21 22:56:52 +0530

    Functions

---
+++ Functions.sql
@@ -0,0 +1,13 @@
+--  functions
+
+SELECT Abs(-243.5) AS AbsNum;
+
+SELECT CEILING(25.75) AS CeilValue;
+
+SELECT FLOOR(25.75) AS FloorValue;
+
+SELECT COUNT(ID) AS NumberOfProducts FROM tblProducts;
+
+SELECT DEGREES(1.5);
+
+SELECT REVERSE('Samarth');
