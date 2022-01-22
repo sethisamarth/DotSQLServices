1cf6582574eb76bca8c39ceee5d4df6fb2678c11
Samarth <sethisamarth1@gmail.com>
2022-01-22 06:33:41 +0530

    added try catch block

--- SQL_StoreProcedure.sql
+++ SQL_StoreProcedure.sql
@@ -1,30 +1,89 @@
 -- Store Procedures
--- creating storeprocedures
+-- creating storeprocedure
 CREATE PROCEDURE SelectAllHiredCandidates
 AS
+BEGIN TRY --BEGIN OF TRY
 SELECT * FROM Hired_Candidates
+END TRY -- END TRY
+BEGIN CATCH --BEGIN OF CATCH
+SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage; 
+END CATCH --END OF CATCH
 GO;
 
+-- creating storeprocedure
 CREATE PROCEDURE SelectAllCandidatesPersonalDetails
 AS
+BEGIN TRY --BEGIN OF TRY
 SELECT * FROM Candidates_Personal_Details
+
+END TRY -- END TRY
+BEGIN CATCH --BEGIN OF CATCH
+SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage; 
+END CATCH --END OF CATCH
 GO;
 
+-- creating storeprocedure
 CREATE PROCEDURE SelectAllFellowshipCandidates
 AS
+BEGIN TRY --BEGIN OF TRY
 SELECT * FROM Fellowship_Candidates
+END TRY -- END TRY
+BEGIN CATCH --BEGIN OF CATCH
+SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage; 
+END CATCH --END OF CATCH
 GO;
 
+-- creating storeprocedure
 CREATE PROCEDURE SelectAllCandidateBankdetails
 AS
+BEGIN TRY --BEGIN OF TRY
 SELECT * FROM Candidate_Bank_details
+END TRY -- END TRY
+BEGIN CATCH  --BEGIN OF CATCH
+SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage; 
+END CATCH --END OF CATCH
 GO;
 
+-- creating storeprocedure
 CREATE PROCEDURE SelectAllCandidateQualification
 AS
+BEGIN TRY --BEGIN OF TRY
 SELECT * FROM Candidate_Qualification
+END TRY -- END TRY
+BEGIN CATCH --BEGIN OF CATCH
+SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage; 
+END CATCH --END OF CATCH
 GO;
-
 
 --executing storeprocedures
 EXEC SelectAllHiredCandidates;
--- sqlcreate.sql
+++ sqlcreate.sql
@@ -56,6 +56,40 @@ create table Fellowship_Candidates(
 	remark varchar(500) not null
 	);
 
+	-- creating felloship candidate table
+create table Fellowship_Candidates_copy(
+	Id int identity(1,1) primary key,
+	first_name varchar(120) not null,
+	last_name varchar(120) not null,
+	emailID varchar(120) unique,
+	hired_date datetime not null,
+	mobile_number int not null unique,
+	permanent_pincode int,
+	hired_lab varchar(50) not null,
+	attitude varchar(120) not null,
+	communication_remark varchar(120) not null,
+	knowledge_remark varchar(120) not null,
+	aggregate_remark varchar(120),
+	creator_stamp varchar(120),
+	creator_user varchar(120) not null,
+	birth_date date not null,
+	birth_date_verified varchar(4) not null,
+	parent_name varchar(120) not null,
+	parent_occupation varchar(120) not null,
+	parents_mobile_number int not null,
+	parents_annual_salery int not null,
+	local_address varchar(500) not null,
+	permanent_address varchar(500) not null,
+	photo_path varchar(120) not null,
+	joining_date datetime not null,
+	candidate_status varchar(20) not null,
+	personal_information varchar(120),
+	bank_information varchar(120) not null,
+	educational_information varchar(120) not null,
+	document_status varchar(100) not null,
+	remark varchar(500) not null
+	);
+
 --creating candidate personal detail table
 create table Candidates_Personal_Details(
     Id int identity(1,1) primary key,
