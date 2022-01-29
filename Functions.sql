09a8f7d3dc012c17f901f7f46ad69638ef15d7b8
Samarth <sethisamarth1@gmail.com>
2022-01-29 09:03:55 +0530

    Added userDefined functions

--- Functions.sql
+++ Functions.sql
@@ -11,3 +11,23 @@ SELECT COUNT(ID) AS NumberOfProducts FROM tblProducts;
 SELECT DEGREES(1.5);
 
 SELECT REVERSE('Samarth');
+
+
+--User defined functions
+CREATE FUNCTION GetProduct(@Id INT)  
+ RETURNS VARCHAR(50)  
+ AS  
+BEGIN  
+    RETURN (SELECT Name FROM tblProducts WHERE Id=@Id)  
+END 
+
+PRINT dbo.GetProduct(1) ;
+
+-- get all productucts user defined functions
+CREATE FUNCTION GetAllProductss(@Id INT)  
+RETURNS TABLE  
+AS  
+RETURN  
+    SELECT *FROM tblProducts WHERE Id<=@Id 
+
+SELECT *FROM GetAllProductss(10) 
